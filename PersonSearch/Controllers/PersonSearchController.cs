894d37209781c23f923752a7dcc0757f67050a4c
Jon Zelenik <jzelenik@truenorthlogic.com>
2016-12-31 14:06:01 -0700

    New repository since I forgot to add a .gitignore last time

---
+++ PersonSearch/Controllers/PersonSearchController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PersonSearch.Controllers
+{
+    public class PersonSearchController : Controller
+    {
+        // GET: PersonSearch
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
