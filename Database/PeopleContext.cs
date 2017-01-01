5c64cd4ed31b4d2f365e38d6e4aaa6337ef59e03
Jon Zelenik <jzelenik@truenorthlogic.com>
2016-12-31 19:58:36 -0700

    Added PeopleContext

---
+++ Database/PeopleContext.cs
@@ -0,0 +1,28 @@
+﻿using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using DataModels;
+
+namespace Database
+{
+    public class PeopleContext : DbContext
+    {
+        public PeopleContext() : base("PeopleContext")
+        {
+        }
+
+        public DbSet<Person> People { get; set; }
+
+        public DbSet<Address> Addresses { get; set; }
+
+        public DbSet<City> Cities { get; set; }
+
+        public DbSet<State> States { get; set; }
+
+        public DbSet<Interest> Interests { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
