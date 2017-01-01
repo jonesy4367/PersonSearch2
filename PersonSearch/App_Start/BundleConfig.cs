545b56451f1eac977c0f8a51c04ba478909f4256
Jon Zelenik <jzelenik@truenorthlogic.com>
2017-01-01 14:42:50 -0700

    Moved javascript to a separate file

--- PersonSearch/App_Start/BundleConfig.cs
+++ PersonSearch/App_Start/BundleConfig.cs
@@ -7,8 +7,8 @@ namespace PersonSearch
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            //bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-            //    "~/Scripts/jquery-{version}.js"));
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                "~/Scripts/jquery-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                 "~/Scripts/jquery.validate*"));
@@ -21,6 +21,9 @@ namespace PersonSearch
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                 "~/Scripts/bootstrap.js",
                 "~/Scripts/respond.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/custom").Include(
+                "~/Scripts/Custom/PersonSearch.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                 "~/Content/bootstrap.css",
---
+++ PersonSearch/Scripts/Custom/PersonSearch.js
@@ -0,0 +1,25 @@
+﻿$(document).ready(function() {
+    $("#searchButton").on("click", function() {
+
+        function convertFormToJson(form) {
+            var formArray = form.serializeArray();
+            var json = {};
+
+            $.each(formArray, function() {
+                json[this.name] = this.value || "";
+            });
+
+            return json;
+        };
+
+        $.ajax({
+            url: "PersonSearch/SearchPeople",
+            type: "POST",
+            contentType: "application/json",
+            data: JSON.stringify(convertFormToJson($("#personSearchForm"))),
+            success: function(result) {
+                console.log(result);
+            }
+        });
+    });
+})
